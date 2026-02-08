f5391b5477c3498ff8b7345539ac7b6abb564cc6
Rowthu Naveen Kumar <naveenkumar910016@gmail.com>
2026-02-08 16:07:34 +0530

    Day 1: Backend setup completed

---
+++ backend/sgms-backend/src/main/java/com/sgms/sgms_backend/SgmsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sgms.sgms_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgmsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgmsBackendApplication.class, args);
+	}
+
+}
